8ca8b74e19c4095d7fad02237d52615c6b8f2553
Kernel_Panic <ashwinfrancisjoseph@gmail.com>
2022-07-27 12:13:10 +0800

    Create IP_Output.py

---
+++ IP_Output.py
@@ -0,0 +1,12 @@
+import subprocess
+import re
+
+subprocess.call("ifconfig > if_out.txt", shell=True)
+interface_rule = re.compile(r'(.*?):\W')
+ip_rule = re.compile(r'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+with open('if_out.txt', 'r') as f:
+  content = f.read()
+  ip_address = re.findall(ip_rule, content)
+  interface = re.findall(interface_rule, content)
+  for i in range(len(ip_address)):
+    print(interface[i] + ": " + ip_address[i][4:])
