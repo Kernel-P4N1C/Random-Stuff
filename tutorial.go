753a7567022a101338143114e0cc931e193a0c0d
Kernel_Panic <ashwinfrancisjoseph@gmail.com>
2022-12-26 14:13:03 +0800

    Add files via upload

---
+++ tutorial.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Welcome to my quiz game!")
+
+    var name string
+    fmt.Printf("enter your name")
+    fmt.Scan(&name)
+    fmt.Println(name)
+
+}
