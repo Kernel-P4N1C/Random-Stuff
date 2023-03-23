0d478dace36b46307e1780ee1e081ec8e589b737
Kernel_Panic <ashwinfrancisjoseph@gmail.com>
2023-03-23 17:21:02 +0800

    Update IP_Output.py

--- IP_Output.py
+++ IP_Output.py
@@ -9,4 +9,6 @@ with open('if_out.txt', 'r') as f:
   ip_address = re.findall(ip_rule, content)
   interface = re.findall(interface_rule, content)
   for i in range(len(ip_address)):
-    print(interface[i] + ": " + ip_address[i][4:])
+    print(interface[i].strip() + ": " + ip_address[i][4:])
+  f.close()
+subprocess.call("rm if_out.txt", shell=True)
